8cef5ba1cdc782809890a4426f867d7046bc5a76
Jonathon Duvall <181755031+ElvLegion@users.noreply.github.com>
2026-09-21 11:34:18 -0500

    Add Tailscale cloud-init bootstrap template

--- terraform/azure/locals.tf
+++ terraform/azure/locals.tf
@@ -6,4 +6,10 @@ locals {
     ManagedBy   = "Terraform"
     Workload    = "Utility"
   }
+
+  bootstrap_cloud_init = templatefile("${path.module}/cloud-init.yaml.tftpl", {
+    bootstrap_identity_client_id = azurerm_user_assigned_identity.bootstrap.client_id
+    key_vault_name               = azurerm_key_vault.bootstrap.name
+    tailscale_hostname           = "vm-hmlb-util"
+  })
 }
