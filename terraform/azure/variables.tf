f533b8fc9ee59098f56db29b09f8abfaf94d34c9
ElvLegion <YOUR_GITHUB_NOREPLY_EMAIL>
2026-09-22 09:57:44 -0500

    Add Azure security monitoring and alerting

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -6,3 +6,8 @@ variable "admin_ssh_public_key" {
   description = "SSH public key for the utility VM administrator"
   type        = string
 }
+variable "alert_email" {
+  description = "Email address for Azure security alert notifications"
+  type        = string
+  sensitive   = true
+}
